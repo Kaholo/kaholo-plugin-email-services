85f0a9e1552fde09add6e0098f93b9662f3544e3
xkcm <xkcm16@gmail.com>
2022-06-22 21:47:45 +0200

    Add "Send Mail" method

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-function hello(name) {
-  const greeting = `hello ${name}`;
-  return greeting;
+const kaholoPluginLibrary = require("@kaholo/plugin-library");
+const nodemailerSendgrid = require("nodemailer-sendgrid");
+const nodemailer = require("nodemailer");
+const { sendWithTransport } = require("./mail-service");
+
+function send({
+  service,
+  username,
+  password,
+  apiKey,
+  ...mailingDetails
+}) {
+  let transportCreationOptions;
+
+  if (service === "SendGrid") {
+    transportCreationOptions = nodemailerSendgrid({
+      apiKey,
+    });
+  } else {
+    transportCreationOptions = { service };
+
+    if (apiKey) {
+      transportCreationOptions.auth = { api_key: apiKey };
+    } else {
+      transportCreationOptions.auth = {
+        user: username,
+        pass: password,
+      };
+    }
+  }
+
+  const transport = nodemailer.createTransport(transportCreationOptions);
+  return sendWithTransport(transport, mailingDetails);
 }
 
-module.exports = {
-  hello,
-};
+module.exports = kaholoPluginLibrary.bootstrap({
+  send,
+});
