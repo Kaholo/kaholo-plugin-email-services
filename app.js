9a615e387d841aa4027a2d4bc094ed90f7e896d6
maciek <42122011+xkcm@users.noreply.github.com>
2022-06-28 14:05:42 +0200

    KP-787 Version 1.0.0 (#1)

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-function hello(name) {
-  const greeting = `hello ${name}`;
-  return greeting;
+const kaholoPluginLibrary = require("@kaholo/plugin-library");
+const MailService = require("./mail-service");
+
+function send({
+  service,
+  username,
+  password,
+  apiKey,
+  to,
+  from,
+  subject,
+  cc,
+  bcc,
+  text,
+  html,
+  attachmentPaths,
+}) {
+  const mailService = new MailService(service, username, password, apiKey);
+
+  return mailService.send({
+    to,
+    from,
+    subject,
+    cc,
+    bcc,
+    text,
+    html,
+    attachmentPaths,
+  });
 }
 
-module.exports = {
-  hello,
-};
+module.exports = kaholoPluginLibrary.bootstrap({
+  send,
+});
---
+++ mail-service.js
@@ -0,0 +1,74 @@
+const nodemailerSendgrid = require("nodemailer-sendgrid");
+const nodemailer = require("nodemailer");
+const path = require("path");
+
+const NEW_LINE_WITH_OPTIONAL_COMMA_REGEX = /(?:,\s*)?\n/g;
+const COMMA_WITH_WHITESPACE_REGEX = /\s*,\s*/g;
+
+class MailService {
+  static FIELDS_WITH_MULTILINE_SUPPORT = ["bcc", "to", "cc"];
+
+  constructor(service, username, password, apiKey) {
+    let transportCreationOptions;
+
+    if (service === "SendGrid") {
+      transportCreationOptions = nodemailerSendgrid({
+        apiKey,
+      });
+    } else {
+      transportCreationOptions = { service };
+
+      if (apiKey) {
+        transportCreationOptions.auth = { api_key: apiKey };
+      } else {
+        transportCreationOptions.auth = {
+          user: username,
+          pass: password,
+        };
+      }
+    }
+
+    this.transport = nodemailer.createTransport(transportCreationOptions);
+  }
+
+  send({
+    to,
+    from,
+    subject,
+    cc,
+    bcc,
+    text,
+    html,
+    attachmentPaths,
+  }) {
+    const correctedMailOptions = {
+      to,
+      from,
+      subject,
+      cc,
+      bcc,
+      text,
+      html,
+    };
+
+    MailService.FIELDS_WITH_MULTILINE_SUPPORT.forEach((field) => {
+      if (correctedMailOptions[field]) {
+        correctedMailOptions[field] = correctedMailOptions[field]
+          .trim()
+          .replace(NEW_LINE_WITH_OPTIONAL_COMMA_REGEX, ", ")
+          .replace(COMMA_WITH_WHITESPACE_REGEX, ", ");
+      }
+    });
+
+    if (attachmentPaths.length) {
+      correctedMailOptions.attachments = attachmentPaths.map((attachmentPath) => ({
+        filename: path.basename(attachmentPath),
+        path: attachmentPath,
+      }));
+    }
+
+    return this.transport.sendMail(correctedMailOptions);
+  }
+}
+
+module.exports = MailService;
